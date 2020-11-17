1fa81a98fadbff9a3a8ec3df7690ce331b97a140
Shingirirai5 <71317833+Shingirirai5@users.noreply.github.com>
2020-11-17 15:20:50 +0200

    final

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 const flash = require('connect-flash');
+const xlsx = require('xlsx');
+const pdfparse = require('pdf-parse');
+const fs = require('fs');
 const session = require('express-session');
 
 const app = express();
@@ -26,6 +30,8 @@ mongoose
 
 // EJS
 app.use(expressLayouts);
+//app.use(bodyParser.json());
+//app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 // Express body parser
@@ -59,6 +65,226 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
+//Jason web token
+/*app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1, 
+    username: 'shingie',
+    email: 'mherembi@gmail.com'
+  }
+
+  jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+*/
+
+//Upload files to database
+/*
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+// @route GET /
+// @desc Loads form
+app.get('/', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'image/jpeg' ||
+          file.contentType === 'image/png'
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render('index', { files: files });
+    }
+  });
+});
+
+// @route POST /upload
+// @desc  Uploads file to DB
+app.post('/upload', upload.single('file'), (req, res) => {
+  // res.json({ file: req.file });
+  res.redirect('/');
+});
+
+// @route GET /files
+// @desc  Display all files in JSON
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
+
+    // Files exist
+    return res.json(files);
+  });
+});
+
+// @route GET /files/:filename
+// @desc  Display single file object
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+    // File exists
+    return res.json(file);
+  });
+});
+
+// @route GET /image/:filename
+// @desc Display Image
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
+
+// @route DELETE /files/:id
+// @desc  Delete file
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect('/');
+  });
+});*/
+
+const pdffile = fs.readFileSync('sample__.pdf')
+//Classification
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value.match(mailformat))
+{
+   return true;
+}
+}
+ //classify phone
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if((inputtxt.value.match(phoneno)))
+  {
+      return true;
+    }
+}
+
+//classify dob
+function DateOfBirth(inputText)
+{
+var dobformat = '\d{4}-\d{2}-\d{2}';
+if(inputText.value.match(dobformat))
+{
+   return true;
+}
+}
+
+//id luhns algorithm
+function IDluhnCheck(inputText)
+{
+	if(innerText[i].length==13)
+	{
+		const luhnCheck = num => {
+		let arr = (num + '')
+		.split('')
+		.reverse()
+		.map(x => parseInt(x));
+		let lastDigit = arr.splice(0, 1)[0];
+		let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+		sum += lastDigit;
+		return sum % 10 === 0;}
+    }
+}
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
+
+//classifiy info
+pdfparse(pdffile).then(function(data){
+  console.log(data.text)
+})
+
+
